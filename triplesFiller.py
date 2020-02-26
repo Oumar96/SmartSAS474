1d6e23089b33bef56a1e7c022516c90f60d5748d
Yassine <larrozzi@gmail.com>
2020-02-25 19:28:33 -0500

    subclasses added to the turtle file

--- triplesFiller.py
+++ triplesFiller.py
@@ -9,16 +9,23 @@
 Prefixes = {'ex':'<http://example.org/>', 'owl':'<https://www.w3.org/2002/07/owl#>', 'foaf':'<http://xmlns.com/foaf/0.1/>', 'rdfs':'<https://www.w3.org/2000/01/rdf-schema#>', 'rdf':'<https://www.w3.org/1999/02/22-rdf-syntax-ns#>', 'focu':'<http://focu.io/schema#>', 'xsd':'<http://www.w3.org/2001/XMLSchema#>'}
 
 #classes
-classes=['focu:University','focu:Course','focu:Student','focu:Grade']
+classes=['focu:Course','focu:Grade','focu:courseTopics']
+subclasses={'focu:University':'foaf:Organization','focu:Student':'foaf:Person'}
 props=['foaf:name', 'foaf:schoolHomepage','foaf:familyName','foaf:givenName','foaf:mbox','focu:IDNumber', 'focu:CourseName', 'focu:CourseSubject', 'focu:courseNumber','focus:courseDescription']
-propsDomRange = {'focu:isEnrolledAt': ['focu:Student','focu:University'], 'focu:completedCourseGrade':['focu:Student','focu:Grade'] ,'focu:takesCourse':['focu:Student','focu:Course']}
+propsDomRange = {'focu:isEnrolledAt': ['focu:Student','focu:University'], 'focu:completedCourseGrade':['focu:Student','focu:Grade'] ,'focu:takesCourse':['focu:Student','focu:Course'],'focu:hasTopics':['focu:courseDescription','focu:courseTopics']}
 triples=''
 
 for key, value in Prefixes.items():
     triples += ("@prefix {}: {} .\n".format(key, value))
 
+triples +='\n'
+
 for c in classes:
     triples += c + ' a rdfs:Class .\n'
+
+for c,sbc in subclasses.items():
+    triples += (c + ' a rdfs:Class ;\n'
+                + '\t rdfs:subClassOf {} .\n'.format(sbc))
 
 for prop in props:
     triples += prop + ' a rdf:Property .\n'
