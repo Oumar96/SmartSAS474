dea1884a7c17daa8f378031b820eb66bcae27cd9
Charles <chanc09@gmail.com>
2020-03-04 21:17:46 -0500

    #1 testing

--- scrapencsgrad.py
+++ scrapencsgrad.py
@@ -10,25 +10,26 @@ def scrap_course_data():
     href = "https://www.concordia.ca/academics/graduate/calendar/current/encs/computer-science-courses.html"
     driver = create_driver()
     driver.get(href)
-    
+
     info = {}
 
     #Selenium hands the page source to Beautiful Soup
-    soup=BeautifulSoup(driver.page_source, 'lxml')   
+    soup=BeautifulSoup(driver.page_source, 'lxml')
     course_div = soup.findAll("div", attrs={"class": "wysiwyg parbase section"})[2]
     coursediv_div = course_div.find("div", attrs={"class": "rte"})
     course_para = coursediv_div.findAll("p")
     # print(course_para)
     for elements in course_para:
         course_span = elements.findAll("span", attrs={"class": "large-text"})
-        # print(course_span)
+        print("span")
+        print(course_span)
         for titles in course_span:
             course_title = titles.findAll("b")
-            # print(course_title)
             if len(course_title) > 0:
-                # print(course_title)
+                print("course title")
+                print(course_title)
                 for title in course_title:
-                    # print("==========")
+                    print("==========")
                     if (checkIfStartsWithCourseNumb(title.text) == True):
                         array_p2 = re.split(r" ", title.text)
                         course_code = array_p2[0] + " " + array_p2[1][:4]
@@ -37,20 +38,21 @@ def scrap_course_data():
                         titledesc=''
                         for word in titleCourseTxt:
                             titledesc+= word +' '
-                        print(titledesc)        
+                        print("title desc")
+                        print(titledesc)
 
     quit_driver(driver)
-        
-        
+
+
                                     # for splittext in array_p1:
                                     #     # print(splittext)
                                     #     course_code = splittext[0]
-                                    #     print(course_code) 
+                                    #     print(course_code)
                     # txt_title = title.replace('<b>',' ')
                     # print(txt_title)
-        
+
         # print("======")
-        # print(elements) 
+        # print(elements)
         # array_p = re.split(r"<p>", elements)
         # print(array_p)
         # bold = elements.find("b")
@@ -61,7 +63,7 @@ def scrap_course_data():
     # quit_driver(driver)
 #     course_p = coursediv_div.text
 #     # print(course_p)
-    
+
     # array_p = re.split(r"\n\n", course_para)
 #     for courses in array_p:
 #         array_p1 = re.split(r"\n\xa0\n", courses)
